cc5c8aa375c46a930683cdf4b0222bdc586acb11
bibek1414 <bbhattarai770@gmail.com>
2024-02-29 23:00:59 +0545

    next

--- pages/api/checkout.js
+++ pages/api/checkout.js
@@ -53,8 +53,8 @@ export default async function handler(req,res) {
     line_items,
     mode: 'payment',
     customer_email: email,
-    success_url: process.env.PUBLIC_URL + '/cart?success=1',
-    cancel_url: process.env.PUBLIC_URL + '/cart?canceled=1',
+    success_url: process.env.NEXT_PUBLIC_URL + '/cart?success=1',
+    cancel_url: process.env.NEXT_PUBLIC_URL + '/cart?canceled=1',
     metadata: {orderId:orderDoc._id.toString()},
     allow_promotion_codes: true,
     shipping_options: [
